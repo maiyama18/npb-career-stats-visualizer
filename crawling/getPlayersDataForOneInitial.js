675e9a84060ae67c32c099c59f1161d934932d0a
maiyamag <maiyamag@yahoo-corp.jp>
2018-11-05 12:23:17 +0900

    選手一覧からスクレイピングできるように変更

---
+++ crawling/getPlayersDataForOneInitial.js
@@ -0,0 +1,38 @@
+const cheerio = require('cheerio');
+const jsonframe = require('jsonframe-cheerio');
+const got = require('got');
+const delay = require('delay')
+const getPlayerData = require('./getPlayerData')
+
+const baseUrl = 'http://npb.jp'
+
+const frameForPlayerPageUrls = {
+  players: {
+    _s: 'a.player_unit_1',
+    _d: [{
+      name: 'dd.name',
+      url: '_parent_ @ href'
+    }]
+  }
+}
+
+const getPlayersDataForOneInitial = async (url) => {
+  const response = await got(url);
+  const $ = cheerio.load(response.body);
+  jsonframe($);
+
+  const { players } = $('body').scrape(frameForPlayerPageUrls);
+  for (let player of players) {
+    console.log(`scraping ${player.name}...`);
+    try {
+      const url = `${baseUrl}${player.url}`;
+      const playerData = await getPlayerData(url);
+      console.log(playerData);
+    } catch (err) {
+      console.error(err);
+    }
+    await delay(1000);
+  }
+}
+
+module.exports = getPlayersDataForOneInitial
--- crawling/index.js
+++ crawling/index.js
@@ -1,13 +1,5 @@
-const getPlayerData = require('./getPlayerData');
-
-const urls = [
-  'http://npb.jp/bis/players/21425116.html',
-  'http://npb.jp/bis/players/61365139.html',
-];
+const getPlayersDataForOneInitial = require('./getPlayersDataForOneInitial');
 
 (async () => {
-  for (let url of urls) {
-    const data = await getPlayerData(url);
-    console.log(JSON.stringify(data));
-  }
+  await getPlayersDataForOneInitial('http://npb.jp/bis/players/all/index_a.html');
 })();
