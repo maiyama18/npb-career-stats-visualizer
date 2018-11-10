480b29ed57ae4ef2fb3e0701271ffceaa225e2fe
m4iyama <myam0118@gmail.com>
2018-11-10 11:01:54 +0900

    upload to firestore

--- crawling/upload/uploadPlayerData.js
+++ crawling/upload/uploadPlayerData.js
@@ -1,25 +1,50 @@
-const uploadPlayerData = async (db, playerData) => {
-  // console.log(playerData);
-  // try {
-  //   console.log(`uploading ${playerData.profile.name}...`);
-  //   const { id } = playerData.profile;
-  //   await db.collection('batting').doc(id).set({
-  //     profile: playerData.profile,
-  //     stats: playerData.battingStats,
-  //   });
+const deepEqual = require('deep-equal');
 
-  //   if (playerData.pitchingStats) {
-  //     await db.collection('pitching').doc(id).set({
-  //       profile: playerData.profile,
-  //       stats: playerData.pitchingStats,
-  //     });
-  //   }
-  //   console.log('success!');
-  // } catch (err) {
-  //   console.log('failed!');
-  //   console.log(playerData);
-  //   console.log(err);
-  // }
+const uploadPlayerData = async (db, playerData) => {
+  try {
+    const { id } = playerData.profile;
+
+    const battingRecord = {
+      profile: playerData.profile,
+      battingStats: playerData.battingStats,
+    };
+    if (await recordUpToDate(battingRecord, db, 'batting')) {
+      console.log('Batting data is already up to date'); 
+    } else {
+      await db.collection('batting').doc(id).set(battingRecord);
+    }
+
+    if (!playerData.pitchingStats) return;
+    const pitchingRecord = {
+      profile: playerData.profile,
+      pitchingStats: playerData.pitchingStats,
+    };
+    if (await recordUpToDate(pitchingRecord, db, 'pitching')) {
+      console.log('Pitching data is already up to date'); 
+    } else {
+      await db.collection('pitching').doc(id).set(pitchingRecord);
+    }
+  } catch (err) {
+    console.log('upload failed!');
+    console.log(err);
+  }
+};
+
+const recordUpToDate = async (record, db, collectionName) => {
+  const { id } = record.profile;
+
+  const existingRecordRef = await db.collection(collectionName).doc(id).get();
+  const existingRecord = existingRecordRef.data();
+  if (!existingRecord) {
+    return false;
+  }
+
+  existingRecord.profile = {
+    ...existingRecord.profile,
+    birthDay: existingRecord.profile.birthDay.toDate(),
+  };
+
+  return deepEqual(record, existingRecord); 
 };
 
 module.exports = uploadPlayerData;
