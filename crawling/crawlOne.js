c5ae5f36807aca3209326e822628f21a8f55e5cd
m4iyama <myam0118@gmail.com>
2018-11-10 15:54:12 +0900

    add function to crawl only one player

---
+++ crawling/crawlOne.js
@@ -0,0 +1,38 @@
+const got = require('got');
+const delay = require('delay');
+const scrapePlayerPage = require('./scrape/scrapePlayerPage');
+const cleansePlayersData = require('./cleanse/cleansePlayerData');
+const uploadStatsToFirestore = require('./upload/uploadStatsToFirestore');
+const uploadProfileToAlgolia = require('./upload/uploadProfileToAlgolia');
+
+const crawlOne = async (url) => {
+  try {
+    console.log(url);
+    const playerResponse = await got(url);
+    const playerData = scrapePlayerPage(playerResponse.body);
+    if (noStats(playerData)) {
+      console.error(`skip ${url} because there is no stats.`);
+      return;
+    }
+    if (tooOld(url)) {
+      console.error(`skip ${url} because the record is too old.`);
+      return;
+    }
+
+    const cleansedPlayerData = cleansePlayersData(playerData, url);
+    await uploadProfileToAlgolia(cleansedPlayerData);
+    await uploadStatsToFirestore(cleansedPlayerData);
+    await delay(500);
+  } catch (err) {
+    console.error(`skip ${url} because unknown error occurs`);
+  }
+};
+
+const tooOld = (playerData) => {
+  return parseInt(playerData.battingStats[0].year) <= 1955;
+};
+const noStats = (playerData) => {
+  return !playerData.battingStats[0];
+};
+
+module.exports = crawlOne;
--- crawling/index.js
+++ crawling/index.js
@@ -1,12 +1,19 @@
 const crawl = require('./crawl.js');
+const crawlOne = require('./crawlOne.js');
 
 const type = process.argv[2];
-if (!['all', 'active'].includes(type)) {
-  console.log('Usage: node index.js (all|active)');
+if (!['all', 'active', 'one'].includes(type)) {
+  console.log('Usage: node index.js (all|active|one <url>)');
   process.exit(1);
 }
 
-const topUrl = `http://npb.jp/bis/players/${type}/index.html`;
+if (type === 'one') {
+  const url = process.argv[3];
+  crawlOne(url)
+    .then(() => console.log('crawlOne finished'));
+} else {
+  const topUrl = `http://npb.jp/bis/players/${type}/index.html`;
+  crawl(topUrl)
+    .then(() => console.log('crawl finished'));
+}
 
-crawl(topUrl)
-  .then(() => console.log('crawl finished'));
