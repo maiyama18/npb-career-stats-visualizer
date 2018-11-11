97424825faa51cff7ee3c417f923212e318e1008
m4iyama <myam0118@gmail.com>
2018-11-12 02:11:34 +0900

    show selected players

--- frontend/src/components/SelectedList.js
+++ frontend/src/components/SelectedList.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { List, Button } from 'semantic-ui-react';
 
-const SelectedList = () => (
+const SelectedList = ({ selected }) => (
   <div>
-    List
+    <List divided verticalAlign='middle'>
+      {selected.map(player => (
+        <List.Item key={player.id}>
+          <List.Content floated='right'>
+            <Button>Remove</Button>
+          </List.Content>
+          <List.Content>
+            {player.profile.name}({player.profile.team})
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
   </div>
 );
 
-export default SelectedList;
+const mapStateToProps = (state) => ({
+  selected: state.select.selected,
+});
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SelectedList);
