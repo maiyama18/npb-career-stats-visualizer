1b77029a59845cca6601d612bdc9ee33ddc743ab
m4iyama <myam0118@gmail.com>
2018-11-12 22:58:30 +0900

    途中

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
+import { connect } from 'react-redux';
 
 const Graph = () => {
   return (
-    <div>
-      Chart
-    </div>
+    <ResponsiveLine 
+      data={}
+      margin={{
+        top: 50,
+        bottom: 50,
+        left: 50,
+        right: 100,
+      }}
+      xScale={{
+        type: 'linear'
+      }}
+      yScale={{
+        type: 'linear'
+      }}
+      axisBottom={{
+        legendOffset: 30
+      }}
+      axisLeft={{
+        legendOffset: -30
+      }}
+      dotColor='inherit:darker(0.3)'
+      dotLabel='y'
+      dotLabelYOffset={-10}
+      animate={true}
+      legends={[
+        {
+          anchor: 'bottom-right',
+          justify: false,
+          itemWidth: 80,
+        }
+      ]}
+    />
   );
 };
 
-export default Graph;
+const mapStateToProps = (state) => {
+  const { xAxis, yAxis, yType } = state.graph;
+  const selected = state.data.selected;
+
+  let data = [];
+  selected.forEach(player => {
+    return {
+      id: player.profile.name
+    }
+  })
+}
+
+export default connect()(Graph);
