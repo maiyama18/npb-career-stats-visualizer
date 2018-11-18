bbe01944fa75b0c8ea64c22d1da603bc33a4c316
m4iyama <myam0118@gmail.com>
2018-11-18 16:31:57 +0900

    add google analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
+import ReactGA from 'react-ga';
 import App from './components/App';
 import store from './redux/store';
+
+ReactGA.initialize('UA-129391439-1');
+ReactGA.pageview('/');
 
 ReactDOM.render(
   <Provider store={store}>
